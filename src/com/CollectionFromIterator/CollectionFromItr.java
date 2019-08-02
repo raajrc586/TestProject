1085cb2647127639d1e1572956e6b8f72527fae3
DELL <DELL@PC.mshome.net>
2019-08-02 10:38:40 +0530

    Collection from Iterable

---
+++ src/com/CollectionFromIterator/CollectionFromItr.java
@@ -0,0 +1,28 @@
+package com.CollectionFromIterator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionFromItr {
+	
+	public static Collection<Integer> getCollectionFromIterator(Iterable<Integer> itr){
+		Collection<Integer> coll = new ArrayList<Integer>();
+		
+		for(int i : itr) {
+			coll.add(i);
+		}
+		
+		return coll;
+	}
+	public static void main(String[] args) 
+    { 
+        Iterable<Integer> i = Arrays.asList(1, 2, 3, 4); 
+        System.out.println("Iterable List : " + i); 
+  
+        Collection<Integer> cn = getCollectionFromIterator(i); 
+        System.out.println("Collection List : " + cn); 
+    }
+
+}
